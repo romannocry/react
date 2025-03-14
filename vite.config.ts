5bfc47294d047c134fe593947c9d04263800ca3d
romannocry <roman.medioni@gmail.com>
2025-03-13 22:17:21 -0400

    pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: "/YOUR_REPOSITORY_NAME",
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
